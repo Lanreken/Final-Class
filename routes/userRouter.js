36861123aa772c6734f879b2705040e83fc75ddd
Lanreken <adelugbaolanrewaju@gmail.com>
2025-09-15 16:41:28 +0100

    added the  get one router

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,9 +1,10 @@
-const { register, getAll } = require("../controller/userController")
+const { register, getAll, getOne } = require("../controller/userController")
 const upload = require("../middleware/multer")
 
 const router = require("express").Router()
 
 router.post("/register",upload.single("profilePicture"), register)
 router.get("/users", getAll)
+router.get("/users/:id", getOne)
 
 module.exports = router
